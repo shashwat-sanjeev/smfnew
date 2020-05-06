7c7c5fba59afa012d6bbe39a8739bd33a18e853a
aak2212 <aakarsh2299@gmail.com>
2020-05-06 11:57:50 +0530

    registration page with video upload option

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ var config = {
   };
   firebase.initializeApp(config);
   
+    var uploader = document.getElementById('uploader');
+          var fileButton = document.getElementById('fileButton');
+
+          fileButton.addEventListener('change', function(e) {
+              var file = e.target.files[0];
+
+              var storageRef = firebase.storage().ref('sweet_gifs/'+file.name);
+
+              var task = storageRef.put(file);
+
+              task.on('state_changed', 
+                  function progress(snapshot) {
+                      var percentage = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                      uploader.value = percentage;
+                  },
+                  function error(err) {
+
+                  },
+                  function complete() {
+
+                  }
+                  
+                  );
+          });
   // Reference messages collection
   var messagesRef = firebase.database().ref('messages');
   
@@ -22,11 +46,13 @@ var config = {
     var name = getInputVal('name');
     var email = getInputVal('email');
     var age = getInputVal('age');
+    var location = getInputVal('location')
     var gender = getInputVal('gender');
     var bio = getInputVal('bio');
     var experience = getInputVal('experience')
+    
     // Save message
-    saveMessage(name, email, age, gender, bio, experience);
+    saveMessage(name, email, age, location, gender, bio, experience);
   
     // Show alert
     document.querySelector('.alert').style.display = 'block';
@@ -46,12 +72,13 @@ var config = {
   }
   
   // Save message to firebase
-  function saveMessage(name, email, age, gender, bio, experience){
+  function saveMessage(name, email, age, location, gender, bio, experience){
     var newMessageRef = messagesRef.push();
     newMessageRef.set({
       name: name,
       email:email,
       age:age,
+      location:location,
       gender:gender,
       bio:bio,
       experience:experience
